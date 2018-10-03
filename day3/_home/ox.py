8409e1bf957f64f11f820f327e6c45530f785a9a
rskula <feday13x>
2018-10-03 15:06:58 +0200

    Message

--- day3/_home/ox.py
+++ day3/_home/ox.py
@@ -5,9 +5,11 @@
 
 def tictactoe(game, turn):
     ##Print the board
-    print('\n'+game[0] + "|" + game[1] + "|" + game[2])
-    print(game[3] + "|" + game[4] + "|" + game[5])
-    print(game[6] + "|" + game[7] + "|" + game[8])
+    def printboard():
+        print('\n'+game[0] + "|" + game[1] + "|" + game[2])
+        print(game[3] + "|" + game[4] + "|" + game[5])
+        print(game[6] + "|" + game[7] + "|" + game[8])
+    printboard()
 
     ##Set player's choice
     i = input('\nPodaj indeks \"' + turn + '\": ')
@@ -16,23 +18,31 @@ def tictactoe(game, turn):
     while game[int(i)] != ' ':
         i = input('Indeks zajety, podaj indeks: ')
 
-    #print(game[0:int(i)] + turn + game[int(i)+1:])
     game = game[0:int(i)] + turn + game[int(i)+1:]
 
     ##Viva Las Vegas
     match = False
+    if game[0] == game[1] == game[2] == turn: match = True
+    if game[3] == game[4] == game[5] == turn: match = True
+    if game[6] == game[7] == game[8] == turn: match = True
+    if game[0] == game[3] == game[6] == turn: match = True
+    if game[1] == game[4] == game[7] == turn: match = True
+    if game[2] == game[5] == game[8] == turn: match = True
+    if game[0] == game[4] == game[8] == turn: match = True
+    if game[2] == game[4] == game[6] == turn: match = True
 
-
-
-    if match: return('\nWygrywa ' + turn + '!')
+    if match:
+        printboard()
+        return('\nWygrywa ' + turn + '!')
     elif game.count(' ') == 0:
-        return('\nPamietasz \"Gry Wojenne\"?')
+        printboard()
+        return '\nPamietasz \"Gry Wojenne\"?'
     else:
 
         #Switch player and go again
         if turn == 'o': turn = 'x'
         else: turn ='o'
-        tictactoe(game, turn)
+        return(tictactoe(game, turn))
 
 ##tictactoe(plansza startowa, pierwszy gracz)
-print(tictactoe('xoxoxoxo ', 'o'))
+print(tictactoe('         ', 'o'))

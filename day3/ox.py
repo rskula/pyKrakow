b22684cadc1750492a35681b852507ae40d94176
rskula <feday13x>
2018-10-03 14:41:50 +0200

    Message

--- day3/ox.py
+++ day3/ox.py
@@ -1,20 +1,38 @@
-# Wydrukuj tablicę
-# Pobierz indeks o
-# Sprawdz czy wygrywa
-# Zmien indeks
+## Wydrukuj tablicę
+## Postaw kolko lub krzyzyk
+## Sprawdz czy gracz wygral
+## Nastepny gracz
 
 def tictactoe(game, turn):
+    ##Print the board
     print('\n'+game[0] + "|" + game[1] + "|" + game[2])
     print(game[3] + "|" + game[4] + "|" + game[5])
     print(game[6] + "|" + game[7] + "|" + game[8])
 
+    ##Set player's choice
     i = input('\nPodaj indeks \"' + turn + '\": ')
     if not i.isdigit(): exit(1)
 
-    if turn == 'o': turn = 'x'
-    else: turn ='o'
+    while game[int(i)] != ' ':
+        i = input('Indeks zajety, podaj indeks: ')
 
-    tictactoe(game, turn)
-    return(tictactoe)
+    #print(game[0:int(i)] + turn + game[int(i)+1:])
+    game = game[0:int(i)] + turn + game[int(i)+1:]
 
-print('|'+tictactoe('         ', 'o')+'|')
+    ##Viva Las Vegas
+    match = False
+
+
+
+    if match: return('\nWygrywa ' + turn + '!')
+    elif game.count(' ') == 0:
+        return('\nPamietasz \"Gry Wojenne\"?')
+    else:
+
+        #Switch player and go again
+        if turn == 'o': turn = 'x'
+        else: turn ='o'
+        tictactoe(game, turn)
+
+##tictactoe(plansza startowa, pierwszy gracz)
+print(tictactoe('xoxoxoxo ', 'o'))
